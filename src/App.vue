ee9cb2a4c4d1cf643ad438fcfdadd469c47bb327
pengteling <pengteling@gmail.com>
2018-04-13 20:58:01 +0800

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-
+    <input type="text" ref="ipt">
+    <my-component ref="child"></my-component>
   </div>
 </template>
 
 <script>
-
+import myComponent from './components/MyComponent'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    myComponent
+  },
+  data () {
+    return {
+      msg: 'Hi, Vue.js!'
+    }
+  },
+  mounted () {
+    console.log(this.$refs.ipt)
+    this.msg = '123'
+    this.$refs.ipt.value = this.msg
+    console.log(this.$refs.child)
+    // var iptDom = document.getElementById('id')
+    // setTimeout(() => {
+    //   this.$refs.ipt.value = this.msg
+    // }, 3000)
+
+    // console.log(this.ref.)
+  }
 }
 </script>
