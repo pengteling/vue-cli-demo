9995549b6f01694e238a87b28f505d4deff9ae5e
pengteling <pengteling@gmail.com>
2018-04-16 20:55:22 +0800

    slot-1

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,18 @@
 <template>
   <div class="container">
-    <transition-group tag="div" name="fadeIn">
-      <div class="slide" v-for="(item,index) in banners" :key="index" v-show="index === curpage">
-        <a :href="item.sourceUrl">
-          <img :src="item.imgUrl" alt="">
-        </a>
-      </div>
-    </transition-group>
+    <son class="classson">
+      父组件上子组件包含的内容
+    </son>
+    <button>按钮</button>
   </div>
 </template>
 
 <script>
+import Son from './components/Son'
 export default {
   name: 'App',
-  // props:[],
-  data () {
-    return {
-      curpage: 0,
-      banners: [
-        {
-          sourceUrl: 'https://i1.mifile.cn/a4/xmad_15234553192824_sTkYv.jpg',
-          imgUrl: 'https://i1.mifile.cn/a4/xmad_15234553192824_sTkYv.jpg'
-        },
-        {
-          sourceUrl: 'https://i1.mifile.cn/a4/xmad_1523852977495_RedjU.jpg',
-          imgUrl: 'https://i1.mifile.cn/a4/xmad_1523852977495_RedjU.jpg'
-        }
-      ]
-    }
-  },
-  mounted () {
-    setInterval(() => {
-      this.curpage++
-      if (this.curpage >= this.banners.length) {
-        this.curpage = 0
-      }
-    }, 3000)
+  components: {
+    Son
   }
-
 }
 </script>
-<style lang="scss">
-.slide{
-  position: absolute;
-  width: 800px;
-  height: 200px;
-  img{
-    max-width: 800px;
-  }
-}
-.fadeIn-enter-active, .fadeIn-leave-active{
-  transition: all 1s;
-}
-.fadeIn-enter,.fadeIn-leave-to{
-  opacity: 0;
-}
-.fadeIn-enter{
-  transform: translate3d(100%,0,0)
-}
-.fadeIn-leave-to{
-  transform: translate3d(-100%,0,0)
-}
-</style>
---
+++ src/components/Son.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    这是子组件 <br>
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default{
+  // name: 'Son'
+}
+</script>
